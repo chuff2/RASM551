70c1a0065564712211bdc9a4895066fcb958c1f2
john hogan <john@johns-MacBook-Pro-5.local>
2016-04-19 19:40:18 -0500

    Changed output in chan_capture from capture_done to set_capture_done and made if(rst_n) be its own if statement

--- chan_capture.sv
+++ chan_capture.sv
@@ -1,4 +1,4 @@
-module chan_capture(clk, rst_n, trig_pos, run_mode, wrt_smpl, armed, capture_done, we, smpl_cnt, trig);
+module chan_capture(clk, rst_n, trig_pos, run_mode, wrt_smpl, armed, set_capture_done, we, smpl_cnt, trig);
 
 parameter ENTRIES = 384,		// defaults to 384 for simulation 12288 for DE0
 	LOG2 = 9;					// Log base 2 of number of entries
@@ -9,7 +9,7 @@ input logic run_mode;		// starts the capture state macine
 input logic wrt_smpl;		// Tells us when to write a sample to RAM
 input logic trig;
 output logic armed;			// Tell the trigger logic when we are ready to start checking for triggers
-output logic capture_done;	// Signal goes high when capture is finished and stays high
+output logic set_capture_done;	// Signal goes high when capture is finished and stays high
 output logic we;					// we signal going to RAM
 output logic [LOG2-1:0] smpl_cnt;	// This will connect to waddr of RAMqueues
 
@@ -29,30 +29,31 @@ always_ff @(posedge clk, negedge rst_n)
 
 // Flip flops to store signals
 always_ff @(posedge clk, negedge rst_n)
-	if(!rst_n || rst_trig_cnt)
+	if(!rst_n)
+		trig_cnt <= 0;
+	else if(rst_trig_cnt)
 		trig_cnt <= 0;
 	else if(inc_trig_cnt)
 		trig_cnt <= trig_cnt + 1'b1;
 
 always_ff @(posedge clk, negedge rst_n)
-	if(!rst_n || rst_smpl_cnt)
+	if(!rst_n)
+		smpl_cnt <= 0;
+	else if(rst_smpl_cnt)
 		smpl_cnt <= 0;
 	else if(inc_smpl_cnt)
 		smpl_cnt <= smpl_cnt + 1'b1;
 
 always_ff @(posedge clk, negedge rst_n)
-	if(!rst_n || clr_armed)
+	if(!rst_n)
+		armed <= 0;
+	else if(clr_armed)
 		armed <= 0;
 	else if(set_armed)
 		armed <= 1;
 		
-//note here that we do not need clr_cap_done because that is handled by the "command processing SM"
-always_ff @(posedge clk, negedge rst_n)
-	if(!rst_n)
-		capture_done <= 0;
-	else if(set_cap_done)
-		capture_done <= 1;
-		
+// We do not control capture_done here. That is set/reset in cmd_cfg	
+	
 always_comb begin
 	// Default outputs
 	rst_smpl_cnt = 0;
