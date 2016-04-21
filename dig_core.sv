b8f51bc16787c5368a0f84119c324bfff808faf3
Conner Austin Huff <chuff2@tux-7.cae.wisc.edu>
2016-04-21 10:50:00 -0500

    dig core is complete

--- dig_core.sv
+++ dig_core.sv
@@ -99,9 +99,9 @@ module dig_core(clk,rst_n,smpl_clk,wrt_smpl, decimator, VIH, VIL, CH1L, CH1H,
 	 .CH5TrigCfg(CH5TrigCfg), .VIH(VIH), .VIL(VIL));
 
   //channel capture
-  chan_capture chan_cap(.clk(clk), .rst_n(rst_n), .trig_pos(trig_pos), .set_capture_done(),
+  chan_capture chan_cap(.clk(clk), .rst_n(rst_n), .trig_pos(trig_pos), .set_capture_done(set_capture_done),
 	.run_mode(TrigCfg[4]), .wrt_smpl(wrt_smpl), .armed(armed), .capture_done(TrigCfg[5]),
-	.we(we), .smpl_cnt(), .trig(triggered));
+	.we(we), .trig(triggered));
 
   //5 instances of channel sample logic TODO
 	channel_sample chan_sample1(.CH_Hff5(CH1Hff5), .CH_Lff5(CH1Lff5), .smpl(wdataCH1),
