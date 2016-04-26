595fff9972473c4a42631e2ad3a0b7e07a142d30
Nick Koxlien <koxlienn@gmail.com>
2016-04-26 11:33:03 -0500

    Added first test case for UART_trig_rx

--- tests/UART_trig_rx_tb.sv
+++ tests/UART_trig_rx_tb.sv
@@ -1,17 +1,47 @@
 module UART_trig_rx_tb();
 
-	logic clk, rst_n, RX, UARTtrig;
-	logic [7:0] match, mask;
+	logic clk, rst_n, RX, trmt, tx_done, UARTtrig;
+	logic [7:0] match, mask, tx_data;
 	logic [15:0] baud_cnt;
 
 
-	UART_trig_rx uart(clk, rst_n, RX, baud_cnt, match, mask, UARTtrig);
-
+	UART_trig_rx uart(.clk(clk), .rst_n(rst_n), .RX(RX), .baud_cnt(baud_cnt), .match(match), .mask(mask), .UARTtrig(UARTtrig));
+	uart_tx(.clk(clk), .rst_n(rst_n), .trmt(trmt), .tx_data(tx_data), .TX(RX), .tx_done(tx_done));
+	
 	initial begin
-		rst_n = 1'b1;
+		rst_n = 1'b0;
 		clk = 1'b0;	
-		RX = 1'b0;
+		trmt = 1'b0;
+		match = 8'h55;
+		mask = 8'b0;
+		tx_data = 8'h55;
+		baud_cnt = 16'd108;		
+		
+		@(negedge clk);
+		@(negedge clk) begin
+			rst_n = 1'b1;	
+			trmt = 1'b1;
+		end
 
+		@(posedge tx_done) begin
+			if(UARTtrig == 1'b1 ) $display("Success 1\n");
+			else begin
+				@(posedge clk) begin
+					if(UARTtrig == 1'b1) $display("Success 1\n");
+					else begin
+					
+						@(posedge clk) begin
+							if(UARTtrig == 1'b1) $display("Success 1\n");
+							else begin
+								$display("Test 1 failed\n");
+								$stop;
+							end	
+					end
+				end
+			end
+		end
+		
+		
 	end
 
 	always #2 clk = ~clk;
