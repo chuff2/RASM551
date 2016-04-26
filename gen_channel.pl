d1277a196a54ef3323c4c8d3ecf198b9d9f94ae1
Conner Austin Huff <chuff2@tux-70.cae.wisc.edu>
2016-04-26 10:49:45 -0500

    added files that were added to the collateral folder

--- gen_channel.pl
+++ gen_channel.pl
@@ -4,7 +4,7 @@
 ## used to generate CH1..5mem.txt files for Spring '16 project testing
 #############################################
 
-$tstep = 16/65536;		## CH5 will have 16 periods of waveform within 65536 samples
+$tstep = 6.283/1536.0;		## CH5 will have 1 periods of waveform within 384*4 samples
 
 for ($CH=5; $CH>0; $CH--) {
   $t = 0.0;
@@ -12,7 +12,7 @@ for ($CH=5; $CH>0; $CH--) {
   printf "Opening $outname for write\n";
   open(OUTFILE,">$outname") || die "ERROR: Can't open $outname for write\n";
   
-  for ($x=0; $x<65536; $x++) {
+  for ($x=0; $x<8192; $x++) {
     $value = 128 + 127*sin($t);
 	$val = sprintf("%2x",$value);
     printf OUTFILE "\@%x %s\n",$x,$val;
