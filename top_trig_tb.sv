381f39176647af89d02ab0764da0cbeddf9440c4
Nick Daniel Koxlien <nkoxlien@tux-27.cae.wisc.edu>
2016-05-05 01:21:13 -0500

    top_trig_tb now compiles

--- top_trig_tb.sv
+++ top_trig_tb.sv
@@ -33,17 +33,16 @@ module top_trig_tb();
 
 	//TB signals	
 	logic [10:0] channels;
-	logic [5:0] CHtrigCfg;
+	logic [5:0] CHTrigCfg;
 	logic [9:0] prevChannels;	
 
 	CommMaster comm(.clk(REF_CLK), .rst_n(RST_n), .snd_cmd(send_cmd), .cmd(cmd), .TX(commTX), .RX(commRX),
 		.cmd_cmplt(cmd_cmplt), .resp(resp), .resp_cmplt(resp_cmplt), .clr_rdy(clr_rdy));
 	
 	LA_dig top(.clk400MHz(REF_CLK), .RST_n(RST_n), .locked(locked), .VIH_PWM(VIH_PWM), .VIL_PWM(VIL_PWM), 
-		.CH1L(CH1L), .CH1H(CH1H), .CH2L(CH2L), .CH2H(CH2H), .CH3L(CH3L), .CH3H(CH3H), .CH4L(CH4L), .CH4H(CH4H),
-		.CH5L(CH5L), .CH5H(CH5H), .RX(commTX), .TX(), .LED(LED));
+		.CH1L(channels[0]), .CH1H(channels[1]), .CH2L(channels[2]), .CH2H(channels[3]), .CH3L(channels[4]), .CH3H(channels[5]), .CH4L(channels[6]), .CH4H(channels[7]),
+		.CH5L(channels[8]), .CH5H(channels[9]), .RX(commTX), .TX(), .LED(LED));
 	
-	assign channels = {1'b0, CH5H, CH5L, CH4H, CH4L, CH3H, CH3L, CH2H, CH2L, CH1H, CH1L};
 	
 	
 	initial begin
@@ -58,7 +57,10 @@ module top_trig_tb();
 		//Disabling UART and SPI triggering
 		$display("Disabling UART and SPI triggering...\n");
 		cmd = {TRIG_CFG_WR, 8'h03};
-		sendCmd(cmd);
+		
+		send_cmd = 1;
+		@(posedge REF_CLK);
+		@(negedge REF_CLK) send_cmd = 0;
 		
 		@(posedge top.iDIG.cmd_config.send_resp) begin
 			if(top.iDIG.cmd_config.resp != 8'ha5) begin
@@ -83,42 +85,62 @@ module top_trig_tb();
 		
 		force top.iDIG.armed = 1'b1;
 		
-		CHTrigCfg = 5'b0;
+		CHTrigCfg = 6'b0;
 		
-		while(~CHTrigCfg[10])
+		while(~CHTrigCfg[5]) begin
 			
 			cmd = {CH1TrigCfgWr, 3'b0, CHTrigCfg[4:0]};
-		 	sendCmd(cmd);
-			@(posedge clk);
-			@(negedge clk);
+			
+			send_cmd = 1;
+			@(posedge REF_CLK);
+			@(negedge REF_CLK) send_cmd = 0;
+
+			@(posedge REF_CLK);
+			@(negedge REF_CLK);
 			
 			cmd = {CH2TrigCfgWr, 3'b0, CHTrigCfg[4:0]};
-		 	sendCmd(cmd);
-			@(posedge clk);
-			@(negedge clk);
+			
+			send_cmd = 1;
+			@(posedge REF_CLK);
+			@(negedge REF_CLK) send_cmd = 0;
+			
+			@(posedge REF_CLK);
+			@(negedge REF_CLK);
 			
 							
 			cmd = {CH3TrigCfgWr, 3'b0, CHTrigCfg[4:0]};
-		 	sendCmd(cmd);
-			@(posedge clk);
-			@(negedge clk);
+			
+			send_cmd = 1;
+			@(posedge REF_CLK);
+			@(negedge REF_CLK) send_cmd = 0;
+			
+			@(posedge REF_CLK);
+			@(negedge REF_CLK);
 			
 			
 			cmd = {CH4TrigCfgWr, 3'b0, CHTrigCfg[4:0]};
-		 	sendCmd(cmd);
-			@(posedge clk);
-			@(negedge clk);
+			
+			send_cmd = 1;
+			@(posedge REF_CLK);
+			@(negedge REF_CLK) send_cmd = 0;
+			
+			@(posedge REF_CLK);
+			@(negedge REF_CLK);
 			
 			
 			cmd = {CH5TrigCfgWr, 3'b0, CHTrigCfg[4:0]};
-		 	sendCmd(cmd);
-			@(posedge clk);
-			@(negedge clk);
+			
+			send_cmd = 1;
+			@(posedge REF_CLK);
+			@(negedge REF_CLK) send_cmd = 0;
+			
+			@(posedge REF_CLK);
+			@(negedge REF_CLK);
 
 				while(~channels[10]) begin
 			
 					repeat(6) begin
-						@(posedge clk);
+						@(posedge REF_CLK);
 					end
 					
 					//Channel triggering testing block
@@ -130,7 +152,7 @@ module top_trig_tb();
 						end		
 					end	
 					
-					if((top.iDIG.cmd_config.CH1TrigCfg & 5'h02) & ~CH1L) begin
+					if((top.iDIG.cmd_config.CH1TrigCfg & 5'h02) & ~channels[0]) begin
 						
 						if(~top.iDIG.CH1Trig) begin
 							$display("ERROR: Low level set, CH1L is 0 and channel 1 not triggered\n");
@@ -138,7 +160,7 @@ module top_trig_tb();
 						end	
 					end
 					
-					if((top.iDIG.cmd_config.CH1TrigCfg & 5'h04) & CH1H) begin
+					if((top.iDIG.cmd_config.CH1TrigCfg & 5'h04) & channels[1]) begin
 						
 						if(~top.iDIG.CH1Trig) begin
 							$display("ERROR: High level set, CH1H is 1 and channel 1 not triggered\n");
@@ -146,7 +168,7 @@ module top_trig_tb();
 						end	
 					end
 									
-					if((top.iDIG.cmd_config.CH1TrigCfg & 5'h08) & (~CH1L & prevChannels[0])) begin
+					if((top.iDIG.cmd_config.CH1TrigCfg & 5'h08) & (~channels[0] & prevChannels[0])) begin
 						
 						if(~top.iDIG.CH1Trig) begin
 							$display("ERROR: negedge set and channel 1 not triggered when CH1L has negedge\n");
@@ -154,7 +176,7 @@ module top_trig_tb();
 						end	
 					end
 										
-					if((top.iDIG.cmd_config.CH1TrigCfg & 5'h10) & (CH1H & ~prevChannels[0])) begin
+					if((top.iDIG.cmd_config.CH1TrigCfg & 5'h10) & (channels[1] & ~prevChannels[0])) begin
 						
 						if(~top.iDIG.CH1Trig) begin
 							$display("ERROR: posedge set and channel 1 not triggered when CH1H has posedge\n");
@@ -172,7 +194,7 @@ module top_trig_tb();
 						end		
 					end
 					
-					if((top.iDIG.cmd_config.CH2TrigCfg & 5'h02) & ~CH2L) begin
+					if((top.iDIG.cmd_config.CH2TrigCfg & 5'h02) & ~channels[2]) begin
 						
 						if(~top.iDIG.CH2Trig) begin
 							$display("ERROR: Low level set, CH2L is 0 and channel 2 not triggered\n");
@@ -180,7 +202,7 @@ module top_trig_tb();
 						end	
 					end
 					
-					if((top.iDIG.cmd_config.CH2TrigCfg & 5'h04) & CH2H) begin
+					if((top.iDIG.cmd_config.CH2TrigCfg & 5'h04) & channels[3]) begin
 						
 						if(~top.iDIG.CH2Trig) begin
 							$display("ERROR: High level set, CH2H is 1 and channel 2 not triggered\n");
@@ -188,7 +210,7 @@ module top_trig_tb();
 						end	
 					end
 									
-					if((top.iDIG.cmd_config.CH2TrigCfg & 5'h08) & (~CH2L & prevChannels[1])) begin
+					if((top.iDIG.cmd_config.CH2TrigCfg & 5'h08) & (~channels[2] & prevChannels[1])) begin
 						
 						if(~top.iDIG.CH2Trig) begin
 							$display("ERROR: negedge set and channel 2 not triggered when CH2L has negedge\n");
@@ -196,7 +218,7 @@ module top_trig_tb();
 						end	
 					end
 										
-					if((top.iDIG.cmd_config.CH2TrigCfg & 5'h10) & (CH2H & ~prevChannels[1])) begin
+					if((top.iDIG.cmd_config.CH2TrigCfg & 5'h10) & (channels[3] & ~prevChannels[1])) begin
 						
 						if(~top.iDIG.CH2Trig) begin
 							$display("ERROR: posedge set and channel 2 not triggered when CH2H has posedge\n");
@@ -214,7 +236,7 @@ module top_trig_tb();
 						end		
 					end
 					
-					if((top.iDIG.cmd_config.CH3TrigCfg & 5'h02) & ~CH3L) begin
+					if((top.iDIG.cmd_config.CH3TrigCfg & 5'h02) & ~channels[4]) begin
 						
 						if(~top.iDIG.CH3Trig) begin
 							$display("ERROR: Low level set, CH3L is 0 and channel 3 not triggered\n");
@@ -222,7 +244,7 @@ module top_trig_tb();
 						end	
 					end
 					
-					if((top.iDIG.cmd_config.CH3TrigCfg & 5'h04) & CH3H) begin
+					if((top.iDIG.cmd_config.CH3TrigCfg & 5'h04) & channels[5]) begin
 						
 						if(~top.iDIG.CH3Trig) begin
 							$display("ERROR: High level set, CH3H is 1 and channel 3 not triggered\n");
@@ -230,7 +252,7 @@ module top_trig_tb();
 						end	
 					end
 									
-					if((top.iDIG.cmd_config.CH3TrigCfg & 5'h08) & (~CH3L & prevChannels[2])) begin
+					if((top.iDIG.cmd_config.CH3TrigCfg & 5'h08) & (~channels[4] & prevChannels[2])) begin
 						
 						if(~top.iDIG.CH3Trig) begin
 							$display("ERROR: negedge set and channel 3 not triggered when CH3L has negedge\n");
@@ -238,7 +260,7 @@ module top_trig_tb();
 						end	
 					end
 										
-					if((top.iDIG.cmd_config.CH3TrigCfg & 5'h10) & (CH3H & ~prevChannels[2])) begin
+					if((top.iDIG.cmd_config.CH3TrigCfg & 5'h10) & (channels[5] & ~prevChannels[2])) begin
 						
 						if(~top.iDIG.CH3Trig) begin
 							$display("ERROR: posedge set and channel 3 not triggered when CH3H has posedge\n");
@@ -256,7 +278,7 @@ module top_trig_tb();
 						end		
 					end
 					
-					if((top.iDIG.cmd_config.CH4TrigCfg & 5'h02) & ~CH4L) begin
+					if((top.iDIG.cmd_config.CH4TrigCfg & 5'h02) & ~channels[6]) begin
 						
 						if(~top.iDIG.CH4Trig) begin
 							$display("ERROR: Low level set, CH4L is 0 and channel 4 not triggered\n");
@@ -264,7 +286,7 @@ module top_trig_tb();
 						end	
 					end
 					
-					if((top.iDIG.cmd_config.CH4TrigCfg & 5'h04) & CH4H) begin
+					if((top.iDIG.cmd_config.CH4TrigCfg & 5'h04) & channels[7]) begin
 						
 						if(~top.iDIG.CH4Trig) begin
 							$display("ERROR: High level set, CH4H is 1 and channel 4 not triggered\n");
@@ -272,7 +294,7 @@ module top_trig_tb();
 						end	
 					end
 									
-					if((top.iDIG.cmd_config.CH4TrigCfg & 5'h08) & (~CH4L & prevChannels[3])) begin
+					if((top.iDIG.cmd_config.CH4TrigCfg & 5'h08) & (~channels[6] & prevChannels[3])) begin
 						
 						if(~top.iDIG.CH4Trig) begin
 							$display("ERROR: negedge set and channel 4 not triggered when CH4L has negedge\n");
@@ -280,7 +302,7 @@ module top_trig_tb();
 						end	
 					end
 										
-					if((top.iDIG.cmd_config.CH4TrigCfg & 5'h10) & (CH4H & ~prevChannels[3])) begin
+					if((top.iDIG.cmd_config.CH4TrigCfg & 5'h10) & (channels[7] & ~prevChannels[3])) begin
 						
 						if(~top.iDIG.CH4Trig) begin
 							$display("ERROR: posedge set and channel 4 not triggered when CH4H has posedge\n");
@@ -299,7 +321,7 @@ module top_trig_tb();
 						end		
 					end
 					
-					if((top.iDIG.cmd_config.CH5TrigCfg & 5'h02) & ~CH5L) begin
+					if((top.iDIG.cmd_config.CH5TrigCfg & 5'h02) & ~channels[8]) begin
 						
 						if(~top.iDIG.CH5Trig) begin
 							$display("ERROR: Low level set, CH5L is 0 and channel 5 not triggered\n");
@@ -307,7 +329,7 @@ module top_trig_tb();
 						end	
 					end
 					
-					if((top.iDIG.cmd_config.CH5TrigCfg & 5'h04) & CH5H) begin
+					if((top.iDIG.cmd_config.CH5TrigCfg & 5'h04) & channels[9]) begin
 						
 						if(~top.iDIG.CH5Trig) begin
 							$display("ERROR: High level set, CH5H is 1 and channel 5 not triggered\n");
@@ -315,7 +337,7 @@ module top_trig_tb();
 						end	
 					end
 									
-					if((top.iDIG.cmd_config.CH5TrigCfg & 5'h08) & (~CH5L & prevChannels[4])) begin
+					if((top.iDIG.cmd_config.CH5TrigCfg & 5'h08) & (~channels[8] & prevChannels[4])) begin
 						
 						if(~top.iDIG.CH5Trig) begin
 							$display("ERROR: negedge set and channel 5 not triggered when CH5L has negedge\n");
@@ -323,7 +345,7 @@ module top_trig_tb();
 						end	
 					end
 										
-					if((top.iDIG.cmd_config.CH5TrigCfg & 5'h10) & (CH5H & ~prevChannels[4])) begin
+					if((top.iDIG.cmd_config.CH5TrigCfg & 5'h10) & (channels[9] & ~prevChannels[4])) begin
 						
 						if(~top.iDIG.CH5Trig) begin
 							$display("ERROR: posedge set and channel 5 not triggered when CH5H has posedge\n");
@@ -340,10 +362,9 @@ module top_trig_tb();
 				CHTrigCfg = CHTrigCfg + 1;	
 			end
 
-		end
 	end
 	
 	always #1.25 REF_CLK = ~REF_CLK;
 	
-	`include "tb_tasks.sv"
+
 endmodule
